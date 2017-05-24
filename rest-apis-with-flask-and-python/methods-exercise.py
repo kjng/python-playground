741b11d32c05c0738cee1a401771c731973890ab
Kevin Jang <pianistkevinjang@gmail.com>
2017-05-24 09:04:21 -0700

    Complete methods exercise

---
+++ rest-apis-with-flask-and-python/methods-exercise.py
@@ -0,0 +1,7 @@
+# Complete the method by making sure it returns 42.
+def return_42():
+  return 42
+
+# Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_method(num1, num2):
+  return num1 * num2
