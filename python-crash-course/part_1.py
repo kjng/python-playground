8769efe77d23da877b049b5c6c934cb9a00dcdce
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-11 22:51:58 -0700

    Complete chapter 5 exercises

--- python-crash-course/part_1.py
+++ python-crash-course/part_1.py
@@ -219,3 +219,111 @@ for food in simple_foods:
 simple_foods = ('mac and cheese', 'pizza', 'sushi', 'cocktail shrimp', 'salmon')
 for food in simple_foods:
     print(food)
+
+# Conditional Tests
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
+number = 42
+print("Is number == 42? I predict True.")
+print(number == 42)
+print("Is number == 5? I predict False.")
+print(number == 5)
+
+# More Conditional Tests
+print('String' == 'string')
+print('String'.lower() == 'string')
+print(5 == 2 * 2.5)
+print(10 != 11)
+
+# Alien Colors 1
+alien_color = 'red'
+if alien_color == 'green':
+    print('You just earned 5 points!')
+alien_color = 'green'
+if alien_color == 'green':
+    print('You just earned 5 points!')
+
+# Alien Colors 2
+if alien_color == 'green':
+    print('You just earned 5 points')
+if alien_color != 'green':
+    print('You just earned 10 points!')
+
+if alien_color == 'green':
+    print('You just earned 5 points for shooting the alien!')
+else:
+    print('You just earned 10 points!')
+
+# Alien Colors 3
+alien_color = 'red'
+if alien_color == 'green':
+    print('5 points')
+elif alien_color == 'yellow':
+    print('10 points')
+elif alien_color == 'red':
+    print('15 points')
+
+# Stages of Life
+age = 23
+if age < 2:
+    print('You are a baby')
+elif age >= 2 and age < 4:
+    print('You are a toddler')
+elif age >= 4 and age < 13:
+    print('You are a kid')
+elif age >= 13 and age < 20:
+    print('You are a teenager')
+elif age >= 20 and age < 65:
+    print('You are an adult')
+elif age >= 65:
+    print('You are an elder')
+
+# Favorite Fruit
+favorite_fruits = ['blackberries', 'mangoes', 'oranges']
+if 'blackberries' in favorite_fruits:
+    print('You really like blackberries!')
+if 'mangoes' in favorite_fruits:
+    print('You really like mangoes!')
+if 'oranges' in favorite_fruits:
+    print('You really like oranges!')
+if 'pineapples' in favorite_fruits:
+    print('You really like pineapples!')
+if 'watermelons' in favorite_fruits:
+    print('You really like watermelons!')
+
+# Hello Admin
+usernames = ['kevin', 'sharon', 'c0zy', 'kevinjang', 'admin']
+for user in usernames:
+    if user == 'admin':
+        print('Hello admin, would you like to see a status report?')
+    else:
+        print('Hello ' + user + ', thank you for logging in again.')
+
+# No Users
+if not usernames:
+    print('We need to find some users!')
+
+# Checking Usernames
+current_users = ['c0zy', 'keverlasting', 'c0zy911', 'kevinjang', 'kjang']
+new_users = ['kevin', 'jang', 'c0zy', 'baby', 'kevinjang']
+for user in new_users:
+    if user.lower() in current_users:
+        print("You'll need to enter a new username. " + user + " is taken.")
+    else :
+        print('The username, ' + user + ', is available!')
+
+# Ordinal Numbers
+nums = list(range(1, 10))
+for num in nums:
+    if num == 1:
+        print('1st')
+    elif num == 2:
+        print('2nd')
+    elif num == 3:
+        print('3rd')
+    else:
+        print(str(num) + 'th')
