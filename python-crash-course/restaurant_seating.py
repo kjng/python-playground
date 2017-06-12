4d13323fcfb9852b81fa18e36c29ca669e838f46
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-12 12:39:18 -0700

    Complete restaurant_seating

---
+++ python-crash-course/restaurant_seating.py
@@ -0,0 +1,6 @@
+num_people = input("How many people are in your dinner group? ")
+num_people = int(num_people)
+if num_people > 8:
+  print("You'll need to wait for a table.")
+else:
+  print("Great! Your table is ready.")
