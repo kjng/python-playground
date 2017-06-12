9b5d187d8779e3eb3a9a9f58057d6df28d55b436
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-11 19:32:27 -0700

    Add section4

---
+++ rest-apis-with-flask-and-python/flask-apps/section4/code/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Student(Resource):
+  def get(self, name):
+    return {'student': name}
+
+api.add_resource(Student, '/student/<string:name>') # http://localhost:5000/student/Kevin
+
+if __name__ == '__main__':
+  app.run(debug=True)
