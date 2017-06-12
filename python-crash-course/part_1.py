f0c740bbe2746c20f9d65e5e0d65bb9cafad1a75
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-11 22:28:52 -0700

    Complete chapter 4 exercises

--- python-crash-course/part_1.py
+++ python-crash-course/part_1.py
@@ -20,11 +20,13 @@ print(name.upper())
 print(name.title())
 
 # Famous Quote
-print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+print('Albert Einstein once said, "A person who never made a mistake never \
+    tried anything new."')
 
 # Famous Quote 2
 famous_person = 'Albert Einstein'
-message = famous_person + ' once said, "A person who never made a mistake never tried anything new."'
+message = famous_person + ' once said, "A person who never made a mistake never\
+ tried anything new."'
 print(message)
 
 # Stripping Names
@@ -58,7 +60,8 @@ print(message + names[1])
 print(message + names[2])
 
 # Your Own List
-cars = ['Tesla Model S', 'Honda S2000', 'BMW M2', 'Porsche 911', 'Porsche Boxster']
+cars = ['Tesla Model S', 'Honda S2000', 'BMW M2', 'Porsche 911',
+    'Porsche Boxster']
 message = 'I would like to own a '
 print(message + cars[0] + ' car.')
 print(message + cars[1] + ' car.')
@@ -136,3 +139,83 @@ languages.sort(reverse=True)
 print(languages)
 languages.sort()
 print(languages)
+
+# Pizzas
+pizzas = ['Mushroom', 'Hawaiian', 'Pepperoni']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza.")
+print("I really love pizza!")
+
+# Animals
+animals = ['dog', 'cat', 'turtle']
+for animal in animals:
+    print("A " + animal + " would make a great pet.")
+print("Any of these animals would make a great pet!")
+
+# Counting to Twenty
+for value in range(1, 21):
+    print(value)
+
+# One Million
+million_nums = list(range(1, 1000001))
+# for num in million_nums:
+#   print(num)
+
+# Summing a Million
+print(min(million_nums))
+print(max(million_nums))
+print(sum(million_nums))
+
+# Odd Numbers
+odd_nums = list(range(1, 20, 2))
+for num in odd_nums:
+    print(num)
+
+# Threes
+threes = list(range(3, 31, 3))
+for num in threes:
+    print(num)
+
+# Cubes
+cubes = []
+for val in range(1, 11):
+    print(val**3)
+    cubes.append(val**3)
+
+# Cube Comprehension
+cubes = [value**3 for value in range(1, 11)]
+for val in cubes:
+    print(val)
+
+# Slices
+print("The first tree items in the list are: " + str(cubes[:3]))
+print("Three items from the middle of the list are: " + str(cubes[3:6]))
+print("The last three items in the list are: " + str(cubes[-3:]))
+
+# My Pizzas, Your Pizzas
+friend_pizzas = pizzas[:]
+pizzas.append('Sausage')
+friend_pizzas.append('Three Cheese')
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+print("My friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
+
+# More Loops
+foods = ['pizza', 'falafel', 'carrot cake', 'cannoli']
+friends_foods = ['pizza', 'falafel', 'carrot cake', 'ice cream']
+for food in foods:
+    print(food)
+for food in friends_foods:
+    print(food)
+
+# Buffet
+simple_foods = ('mac and cheese', 'pizza', 'prime rib', 'cocktail shrimp',
+    'chicken nuggets')
+for food in simple_foods:
+    print(food)
+simple_foods = ('mac and cheese', 'pizza', 'sushi', 'cocktail shrimp', 'salmon')
+for food in simple_foods:
+    print(food)
