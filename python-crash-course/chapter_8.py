d25af46406abc00bc6b6bf35d9d99399ae2598e9
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-13 08:01:41 -0700

    Move make_albums to new file

--- python-crash-course/chapter_8.py
+++ python-crash-course/chapter_8.py
@@ -19,39 +19,3 @@ def city_country(city, country):
 print(city_country('santiago', 'chile'))
 print(city_country('new jersey', 'united states'))
 print(city_country('london', 'england'))
-
-def make_album(artist, title, num_tracks=''):
-  album = {}
-  if num_tracks:
-    album = {'artist': artist.title(), 'title': title, 'tracks': num_tracks}
-  else:
-    album = {'artist': artist.title(), 'title': title}
-  return album
-print(make_album('taylor swift', '1989'))
-print(make_album('taylor swift', 'Red', 13))
-
-while True:
-  print("(enter 'q' at any time to quit)")
-
-  artist = input('Enter artist name: ')
-  if artist == 'q':
-    break
-
-  title = input('Enter album title: ')
-  if title == 'q':
-    break
-
-  tracks_valid = False
-  while tracks_valid != True:
-    try:
-      tracks = input('Enter number of tracks (optional): ')
-      if tracks == 'q':
-        break
-      if tracks != '':
-        tracks = int(tracks)
-      tracks_valid = True
-    except:
-      print('Enter a valid int!')
-
-  album = make_album(artist, title, tracks)
-  print(album)
