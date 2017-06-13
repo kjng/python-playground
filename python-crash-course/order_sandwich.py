c2071102efa273f2a8e534071c42c0428d74d671
Kevin Jang <pianistkevinjang@gmail.com>
2017-06-13 08:31:05 -0700

    Complete sandwich import exercise

---
+++ python-crash-course/order_sandwich.py
@@ -0,0 +1,11 @@
+import sandwich_function
+from sandwich_function import order_sandwich
+from sandwich_function import order_sandwich as os
+import sandwich_function as sf
+from sandwich_function import *
+
+print(sandwich_function.order_sandwich('peanut butter', 'jelly'))
+print(order_sandwich('peanut butter', 'jelly'))
+print(os('peanut butter', 'jelly'))
+print(sf.order_sandwich('peanut butter', 'jelly'))
+print(order_sandwich('peanut butter', 'jelly'))
---
+++ python-crash-course/sandwich_function.py
@@ -0,0 +1,3 @@
+def order_sandwich(*ingredients):
+  order = "Making a sandwich with the following toppings: " + ", ".join(ingredients)
+  return order
