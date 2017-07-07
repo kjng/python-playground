935b63082af4fbe2e18495f45d9b9c1b40c417e4
Kevin Jang <pianistkevinjang@gmail.com>
2017-07-06 21:28:26 -0400

    Refactor keydown and keyup functions

--- python-crash-course/Alien_Invasion/game_functions.py
+++ python-crash-course/Alien_Invasion/game_functions.py
@@ -6,19 +6,10 @@ def check_events(ship):
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
-
     elif event.type == pygame.KEYDOWN:
-      # Move ship to right
-      if event.key == pygame.K_RIGHT:
-        ship.moving_right = True
-      elif event.key == pygame.K_LEFT:
-        ship.moving_left = True
-
+      check_keydown_events(event, ship)
     elif event.type == pygame.KEYUP:
-      if event.key == pygame.K_RIGHT:
-        ship.moving_right = False
-      elif event.key == pygame.K_LEFT:
-        ship.moving_left = False
+      check_keyup_events(event, ship)
 
 def update_screen(settings, screen, ship):
   """Update screen for each pass of loop"""
@@ -27,3 +18,16 @@ def update_screen(settings, screen, ship):
 
   # Show most recent screen
   pygame.display.flip()
+
+def check_keydown_events(event, ship):
+  # Move ship to right
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = True
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = True
+
+def check_keyup_events(event, ship):
+  if event.key == pygame.K_RIGHT:
+    ship.moving_right = False
+  elif event.key == pygame.K_LEFT:
+    ship.moving_left = False
