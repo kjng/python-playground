f6b1b9579caa9b37ec9f7b0ed0143f056875bdda
Kevin Jang <pianistkevinjang@gmail.com>
2017-11-27 14:55:02 -0500

    Complete evalQuadratic exercise

---
+++ mit-6.00.1/evalQuadratic.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def evalQuadratic(a, b, c, x):
+    '''
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    '''
+    return a * pow(x, 2) + b * x + c
